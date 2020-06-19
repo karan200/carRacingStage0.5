f2c81c1ddfac7223075813d333dc6e1d18906fff
karan <64732336+karan200@users.noreply.github.com>
2020-06-19 17:01:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,11 +2,10 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
-
+var allPlayers;
+var distance = 0;
 var database;
-
 var form, player, game;
-
 
 function setup(){
   canvas = createCanvas(400,400);
@@ -18,4 +17,13 @@ function setup(){
 
 
 function draw(){
+
+   if (playerCount === 4) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear();
+     game.play();
+   }
+
 }
